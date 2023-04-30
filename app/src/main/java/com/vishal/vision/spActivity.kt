a05e7675a8bde9c70843f4293b2a5fa0b69edafa
VISHAL <hardichboy@gmail.com>
2023-05-01 01:42:23 +0530

    first commit

---
+++ app/src/main/java/com/vishal/vision/spActivity.kt
@@ -0,0 +1,11 @@
+package com.vishal.vision
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class spActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sp)
+    }
+}
